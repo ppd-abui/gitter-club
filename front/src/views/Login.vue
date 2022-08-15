3c721fbe0e8fc006172bb40fdd1070704a0f7c30
Chen <hakaseo@outlook.com>
2022-08-15 16:52:47 +0800

    login

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 export default {
   name: "Login"
 }
@@ -12,26 +12,29 @@ export default {
     </el-header>
 <!--Title-->
     <el-main>
-      <div style="text-align: center; font-family: Cinzel; font-size: 30px; position: relative; bottom: 20px">Sign In to GitterClub</div>
+      <div style="text-align: center; font-family: Cinzel; font-size: 30px; position: relative; bottom: 20px">Sign in to GitterClub</div>
 
-      <el-form style="width: 350px; border: 1.5px solid #d1d1d1; border-radius: 20px; background: #f7f7f7; margin: auto;">
+      <el-form ref="ruleFormRef" :model="user" :rules="rules" status-icon style="width: 350px; border: 1.5px solid #d1d1d1; border-radius: 20px; background: #f7f7f7; margin: auto;">
         <div style="margin: 0 0 30px 0"/>
-<!--Username-->
+
         <p style="position: relative; right: 90px; font-size: 14px; text-align: center; margin: auto;">Username</p>
-        <el-form-item>
-          <el-input style="width: 250px; margin: auto"/>
-<!--Password-->
+<!--Username-->
+        <el-form-item prop="account" >
+          <el-input v-model="user.userAccount" type="account" style="width: 250px; margin: auto" autocomplete="off"/>
         </el-form-item>
+
         <p>
           <span style="position: relative; left: 51px; font-size: 14px; text-align: center;">Password</span>
-          <span style="position: relative; left: 140px; color: #1E97D4; font-size: 12px; text-align: center; margin: auto;">forgot password?</span>
+<!--  forgot link-->
+          <el-link style="position: relative; left: 140px; color: #1E97D4; font-size: 12px; text-align: center; margin: auto;">forgot password?</el-link>
         </p>
-        <el-form-item>
-          <el-input style="width: 250px; margin: auto"/>
+<!--Password-->
+        <el-form-item prop="password">
+          <el-input v-model="user.userPassword" type="password" style="width: 250px; margin: auto" autocomplete="off" show-password/>
         </el-form-item>
 <!--sign in button-->
         <el-form-item>
-          <el-button type="primary" style="color: white; font-size: 16px; font-family: 'Calibri'; letter-spacing: 2px; width: 250px; margin: auto;">
+          <el-button type="primary" @click=login(); style="color: white; font-size: 16px; font-family: 'Calibri'; letter-spacing: 2px; width: 250px; margin: auto;">
             sign in
           </el-button>
         </el-form-item>
@@ -39,15 +42,18 @@ export default {
     </el-main>
 <!--Register-->
     <el-footer style="height: 100%;">
-      <el-form style="width: 350px; height: 100px;border: 1.5px solid #d1d1d1; border-radius: 20px; background: #f7f7f7; margin: 50px auto;">
+      <el-form style="width: 350px; height: 80px;border: 1.5px solid #d1d1d1; border-radius: 20px; background: #f7f7f7; margin: 50px auto;">
         <el-form-item style="margin-bottom: 0">
           <div style="width: 100%; height: 70%; font-size: 20px; padding-top: 10px; font-family: Centaur; text-align: center">New to
             <span style="font-family: Cinzel; font-size: 20px">GitterClub?</span>
           </div>
         </el-form-item>
         <el-form-item style="margin-top: 0;">
-          <div style="width: 100%; height: 30%; font-size: 20px; font-family: Centaur; text-align: center;
-                      color: #1E97D4">Create an Account</div>
+          <div style="width: 100%; display: flex">
+<!--  register link-->
+            <el-link style="font-size: 20px; font-family: Centaur; margin: auto;
+                        color: #1E97D4">Create an Account</el-link>
+          </div>
         </el-form-item>
       </el-form>
     </el-footer>
@@ -55,8 +61,45 @@ export default {
   </el-container>
 </template>
 
-<script setup>
+<script lang="ts" setup>
+  import {ref, reactive} from "vue";
+  import request from "../utils/request";
+  import type { FormInstance } from 'element-plus'
+  const ruleFormRef = ref<FormInstance>()
 
+  let user = reactive({
+    userAccount: '',
+    userPassword: ''
+  })
+
+  const checkPass = (rule: any, value: any, callback: any) => {
+    if (value === '') {
+      callback(new Error('Please input the password'))
+    } else {
+      callback()
+    }
+  }
+  const checkAccount = (rule: any, value: any, callback: any) => {
+    if (value === '') {
+      callback(new Error('Please input the username'))
+    } else {
+      callback()
+    }
+  }
+
+  const rules = reactive({
+    account: [{ validator: checkAccount, trigger: 'blur' }],
+    password: [{ validator: checkPass, trigger: 'blur' }],
+  })
+
+  function login(){
+    console.log('login')
+    request.post('/login', user)
+      .then(Res=>{
+        console.log(Res)
+      })
+
+  }
 </script>
 
 <style scoped>
