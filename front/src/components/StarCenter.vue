da7cbf265f5b356da000479678713e485e52f3d6
Chen <hakaseo@outlook.com>
2022-08-16 23:43:26 +0800

    new view and components

---
+++ front/src/components/StarCenter.vue
@@ -0,0 +1,67 @@
+
+<script lang="ts">
+export default {
+  name: "StarCenter"
+}
+</script>
+
+<template>
+  <el-container>
+    <el-main>
+      <el-col :span="20">
+        <el-input style="margin-top:12px;margin-left: 5px"  v-model="input" placeholder="Please input" :suffix-icon="Search"></el-input>
+      </el-col>
+      <el-col :span ="30" v-for = "(item) in tableData" :key = "item.ID" mode = "vertical">
+        <div id = "card">
+          <el-divider>
+            <el-icon><star-filled /></el-icon>
+          </el-divider>
+          <span>
+                My wishes are fools, they shout across thy song, my Master. Let me but listen.
+              </span>
+
+        </div>
+      </el-col>
+    </el-main>
+  </el-container>
+</template>
+
+
+<script lang="ts" setup>
+import router from '../router/index.js'
+import { ref } from 'vue'
+import { Search,HomeFilled,Star } from '@element-plus/icons-vue'
+
+const input = ref("")
+
+//数据
+const tableData = [
+  {
+    date: '2016-05-02',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1518 弄'
+  },
+  {
+    date: '2016-05-04',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1517 弄'
+  },
+  {
+    date: '2016-05-01',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1519 弄'
+  },
+  {
+    date: '2016-05-03',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1516 弄'
+  }
+]
+
+</script>
+
+
+
+<style  scoped>
+
+</style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,9 +1,38 @@
+
 <script>
+import Index from "../components/Index.vue";
+import RepositoryS from "../components/RepositoryS.vue";
 export default {
-  name: 'Home'
+  name: 'Home',
+  components:{
+    Index,
+    RepositoryS
+  }
 }
 </script>
 
 <template>
-  Home
+
+  <div class="common-layout">
+    <el-container style="height: 100%; display: flex">
+      <el-header style="padding: 0"><Index/>
+        </el-header >
+      <el-container>
+        <el-aside style="border-radius: 4px;background-color: azure;width:240px;height:700px" >
+          <div style="font-size:15px">
+            Recent Repositories
+            <el-button style="border-radius: 20px;background-color: aquamarine">new</el-button>
+          </div>
+          <el-input v-model="input" placeholder="Find a repository" />
+          <ul>
+            <li style="font-size:18px"><repository></repository></li>
+            <li style="font-size:18px"><repository></repository></li>
+          </ul>
+        </el-aside>
+        <el-main style="background-color: aliceblue">
+
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
