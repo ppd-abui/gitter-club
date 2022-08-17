25ec70a66db45b741df75c2f1baad7fdc6fcdba7
Chen <hakaseo@outlook.com>
2022-08-17 09:01:11 +0800

    new view and components

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -14,6 +14,7 @@ const router = createRouter({
       path: '/person',
       name: 'person',
       component: () => import('../views/Person.vue'),
+      redirect: '/RepositoryCenter',
       children: [{
         path: '/RepositoryCenter',
         name: 'RepositoryCenter',
